aeda9f3e62e020371b6af369e40e60780add059c
Sergey Mironenko <serg1prime@gmail.com>
2024-04-26 20:10:10 +0300

    Auth-Server: fifth commit

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-import 'dotenv/config';
 import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { authRouter } from './src/routes/auth.routes.js';
-import { userRouter } from './src/routes/user.routes.js';
-import { errorMiddleware } from './src/middlewares/errorMiddleware.js';
 
-const PORT = process.env.SERVER_PORT || 3002;
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-  origin: process.env.CLIENT_HOST,
-  credentials: true,
-}));
-
-app.use(authRouter);
-app.use(userRouter);
-
-app.use(errorMiddleware);
+const app = express()
+const PORT = 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
+
 app.listen(PORT, () => {
-  console.log('Server is running')
-});
+  console.log(`✅ Server is running on port ${PORT}`);
+})
